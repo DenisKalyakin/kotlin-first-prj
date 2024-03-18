cab93b9a5566e4fe51cddb26c95b021a3b5c00b4
Denis Kalyakin <denis.kalyakin999@gmail.com>
2024-03-18 14:26:49 +0200

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("Enter your city…")
-    val cityName = readLine()
-    println("User lives in $cityName")
+//    println("Enter your city…")
+//    val cityName = readLine()
+//    println("User lives in $cityName")
 }
